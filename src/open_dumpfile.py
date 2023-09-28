b9afc34cde22ce9fbd897a41cab9f6019b69f00b
Geoff Bryan <geoff_bryan@internode.on.net>
2023-09-28 15:57:52 +1000

    Initial source for open_dumpfile.py

---
+++ src/open_dumpfile.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+""" 
+Simple code to import file name time and scale file
+"""
+
+import numpy as np
+
+#====================
+# Main Python section
+#====================
+#
+if __name__ == '__main__':
+
+f = open("dumpname.txt","r")
+print(f.readline())
+f.close()
